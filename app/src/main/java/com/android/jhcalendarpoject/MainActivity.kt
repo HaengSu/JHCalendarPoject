a405c0d4289a7a688b4ccb21bbc17ee9bbde6d55
HaengSu <onse3840@gmail.com>
2023-12-06 00:56:09 +0900

    init

--- app/src/main/java/com/android/jhcalendarpoject/MainActivity.kt
+++ app/src/main/java/com/android/jhcalendarpoject/MainActivity.kt
@@ -22,7 +22,6 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             JHCalendarPojectTheme {
-                // A surface container using the 'background' color from the theme
                 BottomNavigation()
             }
         }
